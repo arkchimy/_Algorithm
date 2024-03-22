846385f28cacd472bc6e57d8b42a91f645eb3298
Arkchimy <ajffkeneod12@naver.com>
2024-03-23 00:36:58 +0900

    03_22 코테 문제풀이

---
+++ BackJoon/그리디/1541번_잃어버린괄호/소스.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int result= 0;
+
+	string str,temp="";
+	cin >> str;
+	bool bPlus = true;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (bPlus)
+		{
+			if (str[i] == '-')
+			{
+				bPlus = false;
+				result += stoi(temp);
+				temp = "";
+			}
+			else if (str[i] == '+')
+			{
+				result += stoi(temp);
+				temp = "";
+			}
+			else
+				temp += str[i];
+		}
+		else
+		{
+			if (str[i] == '-')
+			{
+				result -= stoi(temp);
+				temp = "";
+			}
+			else if (str[i] == '+')
+			{
+				result -= stoi(temp);
+				temp = "";
+			}
+			else
+				temp += str[i];
+		}
+		
+	}
+	if(bPlus)
+		result += stoi(temp);
+	else
+		result -= stoi(temp);
+	cout << result;
+}
