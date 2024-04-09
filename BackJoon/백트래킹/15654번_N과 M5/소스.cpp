6e3843416daa79f47ed962eade48df9addf604f7
Arkchimy <ajffkeneod12@naver.com>
2024-04-10 00:02:29 +0900

    [ 백트레킹 ]  pop을 이용한 효율적인 메모리 사용

--- BackJoon/백트래킹/15654번_N과 M5/소스.cpp
+++ BackJoon/백트래킹/15654번_N과 M5/소스.cpp
@@ -1,52 +1,109 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
-#include <string>
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 using namespace std;
+
 int n, m;
-
 vector<int> vec;
-void sol(int idx,vector<bool> visited,vector<int> str)
-{
-    visited[idx] = true;
-    str.emplace_back(vec[idx]);
-    if (str.size() == m)
-    {
-        for (int i = 0; i < str.size(); i++)
-            cout << str[i] << " ";
-        cout << "\n";
+vector<int> result;
 
+void sol(int depth, vector<bool> &visited)
+{
+    if (depth == m)
+    {
+        for (int num : result)
+        {
+            cout << num << " ";
+        }
+        cout << "\n";
         return;
     }
 
-    for (int i =0; i < visited.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        if (visited[i] == false)
-            sol(i, visited, str);
+        if (!visited[i])
+        {
+            visited[i] = true;
+            result.push_back(vec[i]);
+            sol(depth + 1, visited);
+            result.pop_back();
+            visited[i] = false;
+        }
     }
 }
+
 int main()
 {
-    
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     cin >> n >> m;
     vec.resize(n);
-   
 
-    for (int i =0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> vec[i];
     }
+
     sort(vec.begin(), vec.end());
-    for (int i = 0; i < n; i++)
-    {
-        vector<bool> visited(n);
-        vector<int> str;
-        sol(i,visited,str);
-    }
- }
+
+    vector<bool> visited(n, false);
+    sol(0, visited);
+
+    return 0;
+}
+
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <string>
+//
+//
+//using namespace std;
+//int n, m;
+//
+//vector<int> vec;
+//void sol(int idx,vector<bool> visited,vector<int> str)
+//{
+//    visited[idx] = true;
+//    str.emplace_back(vec[idx]);
+//    if (str.size() == m)
+//    {
+//        for (int i = 0; i < str.size(); i++)
+//            cout << str[i] << " ";
+//        cout << "\n";
+//
+//        return;
+//    }
+//
+//    for (int i =0; i < visited.size(); i++)
+//    {
+//        if (visited[i] == false)
+//            sol(i, visited, str);
+//    }
+//}
+//int main()
+//{
+//    
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//
+//    cin >> n >> m;
+//    vec.resize(n);
+//   
+//
+//    for (int i =0; i < n; i++)
+//    {
+//        cin >> vec[i];
+//    }
+//    sort(vec.begin(), vec.end());
+//    for (int i = 0; i < n; i++)
+//    {
+//        vector<bool> visited(n);
+//        vector<int> str;
+//        sol(i,visited,str);
+//    }
+// }
+//
