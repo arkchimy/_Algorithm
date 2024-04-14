044f1af17effc5a262ffbc9d6081e7e23ce09d58
Arkchimy <ajffkeneod12@naver.com>
2024-04-14 19:55:34 +0900

    [다이나믹] 입력과동시에 해결하는게 시간복잡도에 도움됨

---
+++ BackJoon/다이나믹프로그래밍/12865번_평범한 배낭/소스.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> dp(K + 1, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        int weight, value;
+        cin >> weight >> value;
+
+        for (int w = K; w >= weight; --w)
+            dp[w] = max(dp[w], dp[w - weight] + value);
+        
+    }
+
+    cout << dp[K] << '\n';
+
+    return 0;
+}
+
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//
+//    int N, K;
+//    cin >> N >> K;
+//    int W, V;
+//
+//    vector<pair<int, int>> vec;
+//    vec.reserve(N);
+//    while (N--)
+//    {
+//        cin >> W >> V;
+//        vec.emplace_back(W, V);
+//    }
+//    sort(vec.begin(), vec.end(), [](const pair<int, int>& p1, const pair<int, int>& p2)
+//        {
+//        if (p1.first == p2.first)
+//            return p1.second > p2.second;
+//        return p1.first < p2.first;
+//        }
+//     );
+//    int left=0, right = 0;
+//    int weight = 0;
+//    int sum = 0;
+//    int result = sum;
+//    while (left <= right)
+//    {
+//        if (right == vec.size())
+//            break;
+//        if(weight + vec[right].first <= K)
+//        {
+//            weight += vec[right].first;
+//            sum += vec[right].second;
+//            right++;
+//        }
+//        else
+//        {
+//            weight -= vec[left].first;
+//            sum -= vec[left].second;
+//            left++;
+//        }
+//        result = max(result, sum);
+//    }
+//    cout << result;
+//
+//}
