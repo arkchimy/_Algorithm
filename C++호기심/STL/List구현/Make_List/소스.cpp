d2b4119666c320a7ef26eed8e6c1334a4bec8188
Arkchimy <ajffkeneod12@naver.com>
2024-05-19 19:09:21 +0900

    [리스트 구현] reverse 구현

--- C++호기심/STL/List구현/Make_List/소스.cpp
+++ C++호기심/STL/List구현/Make_List/소스.cpp
@@ -1,74 +1,80 @@
 #include <iostream>
 
-
 using namespace std;
 
 template<typename T>
 struct List
 {
-	List<T>()
-		:data(0), next(nullptr)
-	{
+    List<T>()
+        :data(0), next(nullptr)
+    {
 
-	};
-	List* next;
-	T data;
+    };
+    List* next;
+    T data;
 };
 
 typedef List<int> iList;
 
-
 template<typename T>
 void print(List<T>* head)
 {
-	List<T>* current = head;
+    List<T>* current = head;
 
-	while (current != nullptr)
-	{
-		cout << current->data << "\n";
-		current = current->next;
-
-	}
+    while (current != nullptr)
+    {
+        cout << current->data << "\n";
+        current = current->next;
+    }
 }
 
 template<typename T>
 List<T>* reverse(List<T>* head)
 {
-	List<T>* current = head;
-	List<T>* temp = new List<int>();
-	List<T>* front = nullptr;
-	while (current != nullptr)
-	{
-		temp = current->next;
-		temp->data = current->next->data;
-		temp->next = front;
+    List<T>* current = head;
+    List<T>* previous = nullptr;
+    List<T>* next = nullptr;
 
-		front = temp;
-		current = current->next;
-	}
-	
-	return temp;
+    while (current != nullptr)
+    {
+        next = current->next;   // 다음 노드를 저장
+        current->next = previous; // 현재 노드의 next를 이전 노드로 변경
+        previous = current;     // 이전 노드를 현재 노드로 변경
+        current = next;         // 현재 노드를 다음 노드로 변경
+    }
+
+    return previous; // 새로운 head 반환
 };
-
 
 int main()
 {
-	
-	iList* list = new iList();
-	iList* head = list;
+    iList* list = new iList();
+    iList* head = list;
 
-	for (int i = 0; i < 5; i++)
-	{
-		list->data = i;
-		list->next = new iList();
-		
-		list = list->next;
-	}
+    for (int i = 0; i < 5; i++)
+    {
+        list->data = i;
+        if (i < 4) {
+            list->next = new iList();
+            list = list->next;
+        }
+    }
 
-	
-	print<int>(head);
-	head = reverse<int>(head);
-	print<int>(head);
+    cout << "Original list:" << endl;
+    print<int>(head);
 
+    head = reverse<int>(head);
 
+    cout << "Reversed list:" << endl;
+    print<int>(head);
+
+    // 메모리 해제를 위해 리스트의 노드들을 삭제
+    while (head != nullptr)
+    {
+        iList* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
 }
