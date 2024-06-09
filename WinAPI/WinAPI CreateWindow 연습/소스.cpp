e38992b5e941f8940b96d608b86d9755c400d79a
Arkchimy <ajffkeneod12@naver.com>
2024-06-10 00:15:38 +0900

    WM_CHAR 를 사용하여 입력

--- WinAPI/WinAPI CreateWindow 연습/소스.cpp
+++ WinAPI/WinAPI CreateWindow 연습/소스.cpp
@@ -1,63 +1,88 @@
 #include <windows.h>
-#define Width 1024
-#define Height 728
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM  wParam, LPARAM lParam)
+#define Width 1024	
+#define Height 728
+#define Device_Width 2560
+#define Device_Height 1080
+
+const float aspect = float(Width) / Height;
+LPCWSTR title = L"첫번째 API";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPSTR a, int ncmdShow)
 {
-	switch(iMessage) 
+	WNDCLASS wndclass;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = title;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+
+	RegisterClass(&wndclass);
+	
+	HWND hwnd = CreateWindow(
+		title, title, WS_OVERLAPPEDWINDOW | WS_VSCROLL,
+		Device_Width/2 - Width/2, Device_Height / 2 - Height / 2,Width,Height,
+		NULL, NULL,hInstance, NULL
+		);
+
+	MSG msg;
+	if (!hwnd) 
 	{
-		case WM_QUIT:
-			DestroyWindow(hWnd);
-			break;
-		case WM_LBUTTONDOWN:
-			HDC hdc = GetDC(hWnd);
-			TextOut(hdc, 100, 100, L"태어나서 처음이야", 9);
-			ReleaseDC(hWnd, hdc);
-			return 0;
+		MessageBox(nullptr, L"윈도우 생성 실패", L"윈도우 생성 실패", MB_ICONERROR);
+		
+		return -1;
 	}
-	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+	else
+	{
+		ShowWindow(hwnd, ncmdShow);
+		UpdateWindow(hwnd);
+
+		
+
+		while (GetMessage(&msg, 0, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+		}
+	}
+	return (int)msg.wParam;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
-	WNDCLASS Wndclass;
-	LPCWSTR title = L"타이틀 제목";
+	static WCHAR str[256];
+	static int len = 0;
 
-	Wndclass.cbClsExtra = 0;
-	Wndclass.cbWndExtra = 0;
-	Wndclass.hInstance = hInstance;
-	Wndclass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
-	Wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	Wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	Wndclass.lpfnWndProc = WndProc;
-	Wndclass.lpszClassName = title;
-	Wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	Wndclass.lpszMenuName = NULL;
-
-
-	RegisterClass(&Wndclass);
-
-
-	HWND hWnd =
-	CreateWindow(
-		title, title, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, Width, Height,
-		NULL, NULL, hInstance, NULL
-	);
-
-	
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
-
-	if (!hWnd) 
+	switch (iMessage)
 	{
-		MessageBox(NULL, L"윈도우 생성 실패!", L"오류", MB_ICONEXCLAMATION | MB_OK);
-		return 1;
-	}
-	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_LBUTTONDOWN:
+		MessageBeep(MB_OK);
+		return 0;
+	case WM_CHAR:
+		str[len++] = TCHAR(wParam);
+		str[len] = 0;
+		InvalidateRect(hwnd, nullptr, false);
+		return 0;
+		
+	case WM_PAINT:
+		PAINTSTRUCT ps;
+		RECT rt = { 100,100,500,500 };
+		HDC hdc = BeginPaint(hwnd, &ps);
+		DrawText(hdc, str,len + 1, &rt, 0);
+		EndPaint(hwnd, &ps);
 
+		return 0;
+	}
+	return DefWindowProc(hwnd, iMessage, wParam, lParam);
 }
